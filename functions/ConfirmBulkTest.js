a522f70c376996ba2528000d137793a8b2b6f86e
Caleb Thompson <caleb.thompson@mongodb.com>
2024-02-01 14:05:43 -0800

    fieldname typo

--- functions/ConfirmBulkTest.js
+++ functions/ConfirmBulkTest.js
@@ -4,18 +4,19 @@ exports = async function(phoneNumber, message){
     var test_id = message.substring(2);
     console.log(test_id)
     var recipientsCollection = context.services.get("mongodb-atlas").db("WIWS").collection("BulkTestRecipients");
-    var testCollection = context.services.get("mongodb-atlas").db("WIWS").collection("BulkTests");
+    //var testCollection = context.services.get("mongodb-atlas").db("WIWS").collection("BulkTests");
     try {
       const testid = parseInt(test_id);
       
-      // add if we need to test whether the test actully exists. Like if use types wrong number?
+      // add this if we need to test whether the test actully exists. Like if use types wrong number?
       /*var latestTest = await testCollection.find({testId:testid}).toArray();
       if (latestTest==null){
         console.error("latestTest is null (not found). Check ConfirmBulkTest function!");
         return {status: "error", message: ''};
       }*/
       
-      var tester = await recipientsCollection.updateOne({'number':phoneNumber}, {$addToSet:{'testReponses':testid}});
+      var tester = await recipientsCollection.updateOne({'number':phoneNumber}, {$addToSet:{'testResponses':testid}});
+      return {status: "success", message: 'Thank you for confirming.'};
     }
     catch(err){
           console.error(err);
